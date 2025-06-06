81cc983a8b9b648f8bc04ac6c3777896d0405a83
llli1999 <b818888@hotmail.com>
2025-06-06 10:25:40 +0800

    Update upload-agsb-v2.py

--- upload-agsb-v2.py
+++ upload-agsb-v2.py
@@ -31,11 +31,11 @@ DEBUG_LOG = INSTALL_DIR / "python_debug.log"
 CUSTOM_DOMAIN_FILE = INSTALL_DIR / "custom_domain.txt" # 存储最终使用的域名
 
 # ====== 全局可配置参数（可直接在此处修改） ======
-USER_NAME = "kkddytd"         # 用户名
+USER_NAME = "lili"         # 用户名
 UUID = "a91b59b6-ade4-497d-b4e9-88d184c48048"                     # UUID，留空则自动生成
 PORT = 49999                   # Vmess端口，留空或0则自动生成
-DOMAIN = "free.testlrj.ddns-ip.net"                   # 域名，留空则自动获取
-CF_TOKEN = "eyJhIjoiODBmMjY5ZmQ1N2QzNzNiMmMzZTBkODc4ODg1NWM5MzIiLCJ0IjoiZmVhMzBmODUtOGY5OC00ZTVmLTkyZTktMmU2OTk2M2E1YzUyIiwicyI6Ik4yTmlZemxpTlRjdE5UVm1PQzAwTjJZekxXRmpORGt0TVdVNE5HUmtORGN3TldObSJ9"                 # Cloudflare Token，留空则用Quick Tunnel
+DOMAIN = "li.b818.dpdns.org"                   # 域名，留空则自动获取
+CF_TOKEN = "eyJhIjoiN2E5ODlhOThiNzk4ZDVlOTQ4Njk1MTAxMzU0NmJhZTUiLCJ0IjoiMzk1NmQ5MmYtYjEzMC00OTI4LWFjMDItZWE2NzIyMjFlNTcwIiwicyI6IllURmpNekk1WkRjdFlXUXdZUzAwTmpnMUxXSTVNVFV0TnpkbE16SmlaRFEzWkRnMCJ9"                 # Cloudflare Token，留空则用Quick Tunnel
 # =========================================
 
 # 添加命令行参数解析
